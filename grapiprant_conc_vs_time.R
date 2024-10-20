e2da511576abfe2b0f9368844702219ad57b8059
Ian Miller <k09im01@gmail.com>
2024-10-20 12:58:48 -0400

    Pushing changes to main before updating branch.

--- grapiprant_conc_vs_time.R
+++ grapiprant_conc_vs_time.R
@@ -4,17 +4,28 @@ library(ggplot2)
 # load data
 conc_vs_time_df <- read.csv('grapipriant_conc_vs_time.csv')
 
+# set timepoints for x-axis ticks
+x_major_ticks <- c(0, 0.25, 0.5, 1, 2, 4, 8, 12, 24, 48)
+#x_major_ticks <- c(0, 2, 4, 8, 12, 24, 48)
+
+# format tick axis labels
+scale_fun <- function(x) sprintf("%.1f", x)
+
 # plot individuals
 individual_plot <- conc_vs_time_df %>%
   ggplot(aes(x=timepoint, y=plasma_conc_ng)) +
-    geom_point(aes(color=factor(subject_id))) +
-    geom_line(aes(color=factor(subject_id))) +
-    labs(color='Subject ID', x='Timepoint (hrs)', y='Plasma Concentration (ng/mL)') +
+    geom_line(aes(color=factor(subject_id)), size=1) +
+    #geom_point(aes(fill=factor(subject_id)), size=3, pch=21, color='black') +
+    geom_point(aes(color=factor(subject_id)), size=3) +
+    labs(color='Subject ID', fill='Subject ID', x='Timepoint (hrs)', y='Plasma Concentration (ng/mL)') +
+    scale_x_continuous(breaks=x_major_ticks, minor_breaks = NULL, labels=scale_fun) +
+    scale_color_brewer(palette="Paired") +
     theme_light() +
-    theme(axis.text=element_text(size=12),
-          axis.title=element_text(size=14,face="bold"),
-          legend.title=element_text(size=12,face="bold"),
-          legend.text =element_text(size=10)
+    theme(axis.text=element_text(size=20), 
+          axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1),
+          axis.title=element_text(size=26,face="bold"),
+          legend.title=element_text(size=26,face="bold"),
+          legend.text =element_text(size=20)
     )
 individual_plot
 individual_outpath <- 'grapipriant_conc_vs_time_individuals_7x6.pdf'
@@ -32,20 +43,23 @@ group_by_sex_plot <- conc_vs_time_df %>%
     plasma_conc_ng_sd = sd(plasma_conc_ng)
     ) %>%
     ggplot(aes(x=timepoint, y=plasma_conc_ng_mean)) +
-      geom_point(aes(color=sex)) +
+    geom_line(aes(color=factor(sex)), size=1) +
+      geom_point(aes(color=sex), size=3) +
       geom_errorbar(aes(
         ymin = plasma_conc_ng_mean-plasma_conc_ng_sd,
         ymax = plasma_conc_ng_mean+plasma_conc_ng_sd,
         color=sex),
-        width = 1) +
-      geom_line(aes(color=factor(sex))) +
+        width = 2, size=1) +
+      scale_x_continuous(breaks=x_major_ticks, minor_breaks = NULL, labels=scale_fun) +
+      scale_color_brewer(palette="Paired") +
       labs(color='Subject Sex', x='Timepoint (hrs)', y='Plasma Concentration (ng/mL)') +
       theme_light() + 
-      theme(axis.text=element_text(size=12),
-            axis.title=element_text(size=14,face="bold"),
-            legend.title=element_text(size=12,face="bold"),
-            legend.text =element_text(size=10)
-            )
+      theme(axis.text=element_text(size=20), 
+            axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1),
+            axis.title=element_text(size=26,face="bold"),
+            legend.title=element_text(size=26,face="bold"),
+            legend.text =element_text(size=20)
+      )
 
 group_by_sex_plot
 group_by_sex_plot_outpath <- 'grapipriant_conc_vs_time_sex_7x6.pdf'
@@ -64,19 +78,22 @@ combined_group_plot <- conc_vs_time_df %>%
   ) %>%
   mutate(group='Combined') %>%
   ggplot(aes(x=timepoint, y=plasma_conc_ng_mean)) +
-  geom_point(aes(color=group)) +
+  geom_point(aes(color=group), size=3) +
   geom_errorbar(aes(
     ymin = plasma_conc_ng_mean-plasma_conc_ng_sd,
     ymax = plasma_conc_ng_mean+plasma_conc_ng_sd,
     color=group),
-    width = 1) +
-  geom_line(aes(color=group)) +
-  labs(x='Timepoint (hrs)', y='Plasma Concentration (ng/mL)', color='Combined') +
+    width = 2,
+    size=1) +
+  geom_line(aes(color=group), size=1) +
+  scale_x_continuous(breaks=x_major_ticks, minor_breaks = NULL, labels=scale_fun) +
+  labs(x='Timepoint (hrs)', y='Plasma Concentration (ng/mL)', color='Group') +
   theme_light() + 
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14,face="bold"),
-        legend.title=element_text(size=12,face="bold"),
-        legend.text =element_text(size=10)
+  theme(axis.text=element_text(size=20), 
+        axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1),
+        axis.title=element_text(size=26,face="bold"),
+        legend.title=element_text(size=26,face="bold"),
+        legend.text =element_text(size=20)
   ) +
   scale_color_manual(values=c("black"))
 combined_group_plot
